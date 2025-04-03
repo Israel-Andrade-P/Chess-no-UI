bad59800d5cda9ce13057503591aa8b1eba04719
Zel <israel88andrade@gmail.com>
2025-04-03 15:51:17 -0300

    add position

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Position pos = new Position(3, 4);
+
+        System.out.println(pos);
     }
 }
